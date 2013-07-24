e1eec795febbc00382d67e050b2d66b6c1506997
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2013-07-24 17:27:00 +0100

    refactor: tidy red-teaming

---
+++ src/injectrange/classes.py
@@ -0,0 +1,7 @@
+"""Breach class taxonomy.
+
+A breach class names one family of prompt-injection technique that a guard is
+expected to reject. The corpus tags every pattern with exactly one class, and
+the harness reports a per-class result so a regression shows up as a named class
+opening rather than a single opaque score change.
+"""
