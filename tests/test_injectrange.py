2a9524f7380f4289297de11d536b52fc0e7fb393
Tomas Vrba <19589448+thevoodjev@users.noreply.github.com>
2018-01-23 13:27:00 +0100

    build: regression-testing

--- tests/test_injectrange.py
+++ tests/test_injectrange.py
@@ -19,3 +19,17 @@ CORPUS_PATH = os.path.join(SAMPLES, "corpus.json")
 PERMISSIVE = os.path.join(SAMPLES, "guard-permissive.json")
 STRICT = os.path.join(SAMPLES, "guard-strict.json")
 
+
+class ClassesTest(unittest.TestCase):
+    def test_six_classes_in_fixed_order(self):
+        self.assertEqual(len(classes.BREACH_CLASSES), 6)
+        self.assertEqual(classes.CLASS_KEYS[0], "instruction_override")
+        self.assertEqual(classes.CLASS_KEYS[-1], "exfiltration_framing")
+
+    def test_get_class_known_and_unknown(self):
+        self.assertEqual(classes.get_class("role_confusion").title, "Role confusion")
+        with self.assertRaises(KeyError):
+            classes.get_class("nope")
+
+    def test_is_known(self):
+        self.assertTrue(classes.is_known("tool_coercion"))
